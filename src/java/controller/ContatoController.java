fb591bdb9d5f8b32592408d66de6b36d18663315
Leoginski <leonardo.smoginski@gmail.com>
2018-03-29 09:35:24 -0300

    Correção de dependencias

--- src/java/controller/ContatoController.java
+++ src/java/controller/ContatoController.java
@@ -5,10 +5,88 @@
  */
 package controller;
 
+import model.Contato;
+import persistence.ContatoDAO;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import java.lang.ClassNotFoundException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
 /**
  *
  * @author Leoginski
  */
-public class ContatoController {
-    
+public class ContatoController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, ClassNotFoundException {
+        String nome = request.getParameter("textNome");
+        String email = request.getParameter("textEmail");
+        if (nome.equals("") || email.equals("")) {
+            response.sendRedirect("index.jsp");
+        } else {
+            try {
+                Contato contato = new Contato(nome, email);
+                ContatoDAO.getInstance().save(contato);
+                response.sendRedirect("contatoSucesso.jsp");
+            } catch (SQLException ex) {
+                response.sendRedirect("contatoErro.jsp");
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
