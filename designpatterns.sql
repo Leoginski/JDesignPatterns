631f76bf0c096d1b4d6ce07ab84b5316ce66ffc6
Leoginski <leonardo.smoginski@gmail.com>
2018-04-10 09:03:26 -0300

    Novo banco.

--- designpatterns.sql
+++ designpatterns.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 4.7.7
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Generation Time: Apr 04, 2018 at 11:35 PM
--- Server version: 5.7.21-log
--- PHP Version: 5.6.31
+-- Host: 127.0.0.1
+-- Generation Time: 10-Abr-2018 às 14:00
+-- Versão do servidor: 10.1.30-MariaDB
+-- PHP Version: 7.2.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -27,18 +27,53 @@ USE `designpatterns`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `contato`
---
--- Creation: Apr 04, 2018 at 11:34 PM
+-- Estrutura da tabela `contato`
 --
 
 DROP TABLE IF EXISTS `contato`;
-CREATE TABLE IF NOT EXISTS `contato` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `contato` (
+  `id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  `email` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `departamento`
+--
+
+DROP TABLE IF EXISTS `departamento`;
+CREATE TABLE `departamento` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contato`
+--
+ALTER TABLE `contato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
